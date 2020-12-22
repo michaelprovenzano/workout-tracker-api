1b25df81a009bc0c251b29ea87bd6152a58dbc21
Michael Provenzano <mikepmusic@gmail.com>
2020-12-22 17:12:42 -0500

    fixed bug for duplicate logs by first checking if a log exists before adding to database

--- controllers/workoutLogController.js
+++ controllers/workoutLogController.js
@@ -7,6 +7,20 @@ exports.addWorkoutLog = catchAsync(async (req, res) => {
   req.body.created_at = new Date(Date.now());
   if (!req.body.date) req.body.date = req.body.created_at;
 
+  // Check if workout exists first
+  let workoutLog = await db('workout_logs')
+    .where({
+      program_log_id: req.body.program_log_id,
+      program_workout_id: req.body.program_workout_id,
+    })
+    .first();
+
+  if (workoutLog)
+    return res.status(401).json({
+      status: 'fail',
+      data: 'This workout already exists for this program',
+    });
+
   // Get workout_id (necessary for the joining workout data)
   let programWorkout = await db('programs_workouts')
     .where({ program_workout_id: req.body.program_workout_id })
