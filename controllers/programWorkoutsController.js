442c83aeaa639458223dd4fc8974e01d95d05029
Michael Provenzano <mikepmusic@gmail.com>
2020-12-31 09:12:52 -0500

    added a route to update multiple workouts (workout_order only)

--- controllers/programWorkoutsController.js
+++ controllers/programWorkoutsController.js
@@ -1,4 +1,5 @@
 const factory = require('./factoryController');
+const db = require('./databaseController');
 
 exports.addWorkout = factory.addOne('programs_workouts');
 exports.deleteWorkout = factory.deleteById('programs_workouts', true);
@@ -10,3 +11,26 @@ exports.getAllProgramWorkouts = factory.getAll('programs_workouts', false, [
   },
 ]);
 exports.updateWorkout = factory.updateOne('programs_workouts');
+exports.updateMultipleWorkouts = async (req, res) => {
+  // console.log('getting multiple workouts');
+  const workoutsToUpdate = req.body;
+  let query = 'UPDATE programs_workouts SET workout_order = CASE program_workout_id ';
+  let endWhere = 'END WHERE program_workout_id IN(';
+
+  workoutsToUpdate.forEach((workout, i) => {
+    // Parsing the keys will prevent SQL injection attacks
+    let program_workout_id = parseInt(workout.program_workout_id);
+    let workout_order = parseInt(workout.workout_order);
+
+    query = `${query} WHEN ${program_workout_id} THEN ${workout_order}`;
+    endWhere = `${endWhere}${i !== 0 ? ',' : ''}${program_workout_id}`;
+  });
+  query = `${query} ELSE workout_order ${endWhere}) RETURNING *`;
+
+  let workouts = await db.raw(query);
+
+  res.status(200).json({
+    status: 'success',
+    data: workouts.rows,
+  });
+};
--- routes/programWorkoutsRoutes.js
+++ routes/programWorkoutsRoutes.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const programWorkoutsController = require('../controllers/programWorkoutsController');
 
-router.route('/').get(programWorkoutsController.getAllProgramWorkouts);
+router
+  .route('/')
+  .get(programWorkoutsController.getAllProgramWorkouts)
+  .patch(programWorkoutsController.updateMultipleWorkouts);
 
 module.exports = router;
