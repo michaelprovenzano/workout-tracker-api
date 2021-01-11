5088c40ae2dfe1129102a0f03d5aa51e218e6ad0
Michael Provenzano <mikepmusic@gmail.com>
2021-01-11 05:18:02 -0500

    deleted currentWorkout and currentWorkouts stores

--- client/src/redux/currentWorkout/currentWorkout.types.js
+++
@@ -1,7 +0,0 @@
-const currentWorkoutTypes = {
-  SET_CURRENT_WORKOUT: 'SET_CURRENT_WORKOUT',
-  UPDATE_CURRENT_WORKOUT: 'UPDATE_CURRENT_WORKOUT',
-  CLEAR_CURRENT_WORKOUT: 'CLEAR_CURRENT_WORKOUT',
-};
-
-export default currentWorkoutTypes;
